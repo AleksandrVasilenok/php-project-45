980daf54d756f73f86cda317736a09d966f30286
AleksandrVasilenok <sucerlove@yandex.ru>
2022-11-16 17:07:14 +0600

    test3

--- src/Cli.php
+++ src/Cli.php
@@ -5,15 +5,11 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function welcome(): void
+function welcome(): string
 {
     line('Welcome to the Brain Game!');
-
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    return $name;
 }
-//
-//function whatYouName(): string
-//{
-//    $name = prompt('May I have your name?');
-//    line("Hello, %s!", $name);
-//    return $name;
-//}
+
